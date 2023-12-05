11a025da3a7774eff6d48cb970d8efab9ea6ac71
Amirr95 <arrezvaniyan@gmail.com>
2023-12-05 16:37:48 +0330

    logging functionality

---
+++ logs.py
@@ -0,0 +1,18 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    encoding="utf-8",
+    level=logging.INFO,
+    handlers=[
+        RotatingFileHandler(
+            "nabaat.log", maxBytes=512000, backupCount=5
+        ),  # File handler to write logs to a file
+        logging.StreamHandler(),  # Stream handler to display logs in the console
+    ],
+)
+logger = logging.getLogger("NabaatChatExporter")
+logging.getLogger("httpx").setLevel(logging.WARNING)
